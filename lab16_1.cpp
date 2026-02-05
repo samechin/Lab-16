31051700a8b687ab5ec85118823939783c804222
samechin <cj.krittayoch@gmail.com>
2026-02-05 22:22:37 +0700

    finished

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -5,11 +5,8 @@
 using namespace std;
 
 void showData(double *,int,int);
-
 void randData(double *,int,int);
-
 void findRowSum(const double *,double *,int,int);
-
 void findColSum(const double *,double *,int,int);
 
 int main(){
@@ -21,7 +18,7 @@ int main(){
 	double *dPtr = data[0];
 	randData(dPtr,N,M);
 	showData(dPtr,N,M);
-	
+
 	cout << "---------------------------------------------\n";
 	
 	findRowSum(dPtr,sum1,N,M);
@@ -31,4 +28,36 @@ int main(){
 	
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
+}
+
+void randData(double *dPtr, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        *(dPtr + i) = (rand() % 101) / 100.0;
+    }
+}
+
+void showData(double *dPtr, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr + i) << " ";
+        if((i + 1) % M == 0) cout << endl;
+    }
+}
+
+void findRowSum(const double *dPtr, double *sum1, int N, int M){
+    for(int i = 0; i < N; i++){
+        sum1[i] = 0;
+        for(int j = 0; j < M; j++){
+            sum1[i] += *(dPtr + (i * M + j));
+        }
+    }
+}
+
+void findColSum(const double *dPtr, double *sum2, int N, int M){
+    for(int j = 0; j < M; j++){
+        sum2[j] = 0;
+        for(int i = 0; i < N; i++){
+            sum2[j] += *(dPtr + (i * M + j));
+        }
+    }
 }
